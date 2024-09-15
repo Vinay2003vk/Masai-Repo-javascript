e863851f4c7900f45cc30910761f1c51ea6bbc9b
Vinay2003vk <123830191+Vinay2003vk@users.noreply.github.com>
2024-09-15 09:57:55 -0700

    Add files via upload

---
+++ dowhileloop.js
@@ -0,0 +1,20 @@
+//1. **Basic Do-While Loop**:
+//javascript
+// Basic Do-While Loop
+console.log("Do-While Loop:");
+let i = 1;
+do{
+    console.log(i);
+    i++;
+}
+ while (i <= 5){
+
+// Equivalent While Loop
+console.log("While Loop:") 
+}
+
+i = 1;
+while (i <= 5){
+    console.log(i);
+    i++;
+}
